5e74068c78a5df33d01de4dbcb5856363ac4060b
David Siaw <davidsiaw@gmail.com>
2018-09-18 22:48:05 +0900

    support const functions

--- mrubyclass.hpp
+++ mrubyclass.hpp
@@ -203,6 +203,12 @@ public:
 		create_function(name, func, cls, mrb_define_method);
 	}
 
+	template<typename TRet, typename ... TArgs>
+	void bind_instance_method(const std::string& name, TRet(TClass::*func)(TArgs...) const)
+	{
+		create_function(name, func, cls, mrb_define_method);
+	}
+
 };
 
 
