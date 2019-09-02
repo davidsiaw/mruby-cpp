a38e518fa630e719fdce6e38ec63ffaadd9e9255
David Siaw <davidsiaw@gmail.com>
2019-09-02 12:08:40 +0900

    functions experiment

--- mrubyfunction.hpp
+++ mrubyfunction.hpp
@@ -5,9 +5,14 @@ class BaseFunction
 {
 protected:
 	mrb_state* mrb;
-	RProc* proc;
-	BaseFunction(mrb_state* mrb, RProc* proc) : mrb(mrb), proc(proc)
+	std::shared_ptr<RProc> proc;
+	BaseFunction(mrb_state* mrb, std::shared_ptr<RProc> proc) : mrb(mrb), proc(proc)
 	{ }
+
+	std::shared_ptr<RProc> not_owned(RProc* proc)
+	{
+		return std::shared_ptr<RProc>(proc, [](RProc*) {});
+	}
 
 	template<typename TArgHead, typename TArgHead2, typename ... TArgTail>
 	static void push_args(mrb_state* mrb, std::vector<mrb_value>* vector, TArgHead head, TArgHead2 head2, TArgTail ... tail)
@@ -20,6 +25,12 @@ protected:
 	static void push_args(mrb_state* mrb, std::vector<mrb_value>* vector, TArgHead head)
 	{
 		vector->push_back(TypeBinder<TArgHead>::to_mrb_value(mrb, head));
+	}
+	
+public:
+	std::shared_ptr<RProc> get_proc() const
+	{
+		return proc;
 	}
 };
 
@@ -35,7 +46,7 @@ template<typename TRet, typename ... TArgs>
 class Function<TRet(TArgs...)> : public BaseFunction
 {
 public:
-	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, proc)
+	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, not_owned(proc))
 	{ }
 
 	TRet invoke(TArgs... args)
@@ -49,7 +60,7 @@ public:
 			mrb,
 			mrb_funcall_argv(
 				mrb,
-				TypeBinder<RProc*>::to_mrb_value(mrb, proc),
+				TypeBinder<RProc*>::to_mrb_value(mrb, proc.get()),
 				call,
 				argc,
 				&argvector[0]));
@@ -60,7 +71,7 @@ template<typename ... TArgs>
 class Function<void(TArgs...)> : public BaseFunction
 {
 public:
-	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, proc)
+	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, not_owned(proc))
 	{ }
 
 	void invoke(TArgs... args)
@@ -72,7 +83,7 @@ public:
 		mrb_sym call = mrb_intern_cstr(mrb, "call");
 		mrb_funcall_argv(
 			mrb,
-			TypeBinder<RProc*>::to_mrb_value(mrb, proc),
+			TypeBinder<RProc*>::to_mrb_value(mrb, proc.get()),
 			call,
 			argc,
 			&argvector[0]);
@@ -83,7 +94,7 @@ template<typename TRet>
 class Function<TRet()> : public BaseFunction
 {
 public:
-	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, proc)
+	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, not_owned(proc))
 	{ }
 
 	TRet invoke()
@@ -100,8 +111,29 @@ public:
 template<>
 class Function< void() > : public BaseFunction
 {
+	using func_wrapper_t = NativeObject< std::function< void() > >;
+
+	static mrb_value cfunc_for_mrb(mrb_state* mrb, mrb_value self)
+	{
+		mrb_value boxed_obj_wrap = mrb_proc_cfunc_env_get(mrb, 0);
+		auto obj_wrap = TypeBinder<func_wrapper_t>::from_mrb_value(mrb, boxed_obj_wrap);
+		obj_wrap.get_instance()->operator()();
+		return mrb_nil_value();
+	}
+
+	std::shared_ptr<RProc> make_proc(std::function< void() > func)
+	{
+		func_wrapper_t obj_wrap("Object", std::make_shared< std::function<void()> >(func));
+		mrb_value boxed_obj_wrap = TypeBinder<func_wrapper_t>::to_mrb_value(mrb, obj_wrap);
+		RProc* proc_ptr = mrb_proc_new_cfunc_with_env(mrb, cfunc_for_mrb, 1, &boxed_obj_wrap);
+		return not_owned(proc_ptr);
+	}
+
 public:
-	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, proc)
+	Function(mrb_state* mrb, RProc* proc) : BaseFunction(mrb, not_owned(proc))
+	{ }
+
+	Function(mrb_state* mrb, std::function< void() > func) : BaseFunction(mrb, make_proc(func))
 	{ }
 
 	void invoke()
@@ -109,7 +141,7 @@ public:
 		mrb_value no = mrb_nil_value();
 
 		mrb_sym call = mrb_intern_cstr(mrb, "call");
-		mrb_funcall_argv(mrb, TypeBinder<RProc*>::to_mrb_value(mrb, proc), call, 0, &no);
+		mrb_funcall_argv(mrb, TypeBinder<RProc*>::to_mrb_value(mrb, proc.get()), call, 0, &no);
 	}
 };
 #endif // __MRUBYFUNCTION_HPP__
